90c2fae7ec52d8ab00d60594f5bec2e587f134c8
Borislav Hristov <bofengejn@gmail.com>
2024-05-17 16:41:52 +0300

    Add labels and style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,37 +3,62 @@ import { timeDays, timeWeek, timeYear, timeMonths } from 'd3-time';
 import React, { useEffect } from 'react';
 
 const Home = () => {
-  const width = 800;
-  const height = 600;
+  const width = 1000;
+  const height = 800;
   const cellSize = 20;
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
   useEffect(() => {
     const svg = select("body")
       .append("svg")
       .attr("width", width)
-      .attr("height", height);
+      .attr("height", height)
+      .style("display", "block")
+      .style("margin", "auto");
 
     const yearData = timeDays(new Date(2024, 0, 1), new Date(2024, 11, 31));
 
+    const monthColors = [
+      '#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00',
+      '#ffff33', '#a65628', '#f781bf', '#999999', '#a6cee3',
+      '#1f78b4', '#b2df8a'
+    ];
+
     function renderCalendar(data) {
-      const squares = svg.selectAll("rect")
-        .data(data)
+      // Add day labels
+      svg.selectAll(".dayLabel")
+        .data(daysOfWeek)
         .enter()
-        .append("rect")
-        .attr("width", cellSize)
-        .attr("height", cellSize)
-        .attr("x", d => timeWeek.count(timeYear(d), d) * cellSize)
-        .attr("y", d => d.getDay() * cellSize)
-        .attr("fill", "lightgray")
-        .attr("stroke", "white");
-      const months = svg.selectAll(".month")
+        .append("text")
+        .text(d => d)
+        .attr("x", 0)
+        .attr("y", (d, i) => (i + 1) * cellSize)
+        .attr("dy", "-0.5em")
+        .attr("class", "dayLabel");
+
+      // Add month labels
+      svg.selectAll(".monthLabel")
         .data(timeMonths(new Date(2024, 0, 1), new Date(2024, 11, 31)))
         .enter()
         .append("text")
-        .text(d => d.toLocaleDateString('en-US', { month: 'long' }))
-        .attr("x", d => timeWeek.count(timeYear(d), d) * cellSize)
-        .attr("y", -5)
-        .attr("class", "month");
+        .text(d => d.toLocaleDateString('en-US', { month: 'short' }))
+        .attr("x", (d, i) => (i + 1) * cellSize * 4 + cellSize * 3) // Adjusted position
+        .attr("y", cellSize) // Moved down
+        .attr("class", "monthLabel");
+
+      // Render the calendar squares
+      const squares = svg.selectAll(".day")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "day")
+        .attr("width", cellSize)
+        .attr("height", cellSize)
+        .attr("x", d => (timeWeek.count(timeYear(d), d) % 53) * cellSize + cellSize * 3)
+        .attr("y", d => d.getDay() * cellSize + cellSize * 2) // Adjusted position
+        .attr("fill", d => monthColors[d.getMonth()])
+        .attr("stroke", "white");
+
     }
 
     renderCalendar(yearData);
@@ -56,7 +81,7 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Hi</h1>
+      <h1 style={{ textAlign: 'center' }}>Hi</h1>
     </div>
   );
 }
